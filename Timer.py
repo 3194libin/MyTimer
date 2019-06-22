d9615f0e18f8075a11b1ed29e5edc8d70e7743e8
3194libin <15764747737@qq.com>
2019-06-22 15:31:45 +0800

    Timer

--- Timer.py
+++ Timer.py
@@ -1,7 +1,12 @@
 import time as t
 
-
 class MyTimer:
+
+    def __init__(self):
+        self.prompt = '未开始计时'
+        self.lasted = []
+        self.begin = 0
+        self.end = 0
 
     def __str__(self):
         return self.prompt
@@ -9,7 +14,7 @@ class MyTimer:
     __repr__ = __str__
 
     def start(self):
-        self.start = t.localtime()
+        self.begin = t.localtime()
         print('计时开始。。。')
 
     def stop(self):
@@ -21,6 +26,7 @@ class MyTimer:
         self.lasted = []
         self.prompt = '总共运行了'
         for index in range(6):
-            self.lasted.append(self.stop[index] - self.start[index])
+            self.lasted.append(self.stop[index] - self.begin[index])
             self.prompt += str(self.lasted[index])
+
 
